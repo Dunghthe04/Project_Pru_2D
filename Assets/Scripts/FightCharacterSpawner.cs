939ec88fe9a536b5c820c9d5f3b70966991aa837
Loc <ducloc110805@gmail.com>
2026-03-09 15:03:08 +0700

    Animation and spawn character(only map 2)

--- Assets/Scripts/FightCharacterSpawner.cs
+++ Assets/Scripts/FightCharacterSpawner.cs
@@ -9,18 +9,24 @@ public class FightCharacterSpawner : MonoBehaviour
     [Header("Character prefabs list (same order as SelectScene)")]
     public GameObject[] characterPrefabs; // size = 5
 
+    public float characterScale = 3f;
+
     void Start()
     {
         int p1 = GameSettings.I != null ? GameSettings.I.p1CharIndex : 0;
         int p2 = GameSettings.I != null ? GameSettings.I.p2CharIndex : 0;
 
-        if (characterPrefabs == null || characterPrefabs.Length == 0)
-        {
-            Debug.LogError("Chưa gán characterPrefabs trong Inspector!");
-            return;
-        }
+        GameObject player1 =
+            Instantiate(characterPrefabs[p1], p1Spawn.position, Quaternion.identity);
 
-        Instantiate(characterPrefabs[p1], p1Spawn.position, Quaternion.identity);
-        Instantiate(characterPrefabs[p2], p2Spawn.position, Quaternion.identity);
+        GameObject player2 =
+            Instantiate(characterPrefabs[p2], p2Spawn.position, Quaternion.identity);
+
+        // scale nhân vật
+        player1.transform.localScale = Vector3.one * characterScale;
+        player2.transform.localScale = new Vector3(-characterScale, characterScale, characterScale);
+
+        player1.GetComponent<FighterController>().playerID = 1;
+        player2.GetComponent<FighterController>().playerID = 2;
     }
 }
